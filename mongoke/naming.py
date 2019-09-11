af27cd602b7fba39ce2cc511f44dab7288d01c36
remorses <beats.by.morse@gmail.com>
2019-09-11 18:06:58 +0200

    mongoke: blacked

--- mongoke/naming.py
+++ mongoke/naming.py
@@ -2,17 +2,18 @@ from funcy import collecting
 
 
 def get_relation_filename(relation):
-    fromType = relation['from']
-    relationName = relation['field']
-    return f'{fromType.lower()}_{relationName}'
+    fromType = relation["from"]
+    relationName = relation["field"]
+    return f"{fromType.lower()}_{relationName}"
+
 
 @collecting
 def get_resolver_filenames(config):
-    for typename, type_config in config.get('types', {}).items():
-        if type_config.get('exposed', True):
+    for typename, type_config in config.get("types", {}).items():
+        if type_config.get("exposed", True):
             yield get_query_name(typename)
-            yield get_query_name(typename) + 's'
-    for relation in config.get('relations', []):
+            yield get_query_name(typename) + "s"
+    for relation in config.get("relations", []):
         yield get_relation_filename(relation)
 
 
--- mongoke/support.py
+++ mongoke/support.py
@@ -3,12 +3,15 @@ import os.path
 import requests
 from funcy import pluck, count, merge
 
+
 def touch(filename, data):
     os.makedirs(os.path.dirname(filename), exist_ok=True)
     with open(filename, "w") as f:
         f.write(data)
 
+
 pretty = lambda x: print(json.dumps(x, indent=4, default=str))
+
 
 def zip_pluck(d, keys, enumerate=False):
     args = [pluck(k, d) for k in keys]
@@ -16,17 +19,18 @@ def zip_pluck(d, keys, enumerate=False):
         args = [count(), *args]
     return zip(*args)
 
+
 def get_skema(config):
-    if 'skema_path' in config:
-        with open(config['skema_path']) as f:
+    if "skema_path" in config:
+        with open(config["skema_path"]) as f:
             return f.read()
-    if config.get('skema'):
-        return config.get('skema')
-    if config.get('skema_url'):
-        r = requests.get(config.get('skema_url'), stream=True)
-        skema = ''
+    if config.get("skema"):
+        return config.get("skema")
+    if config.get("skema_url"):
+        r = requests.get(config.get("skema_url"), stream=True)
+        skema = ""
         while 1:
-            buf = r.raw.read(16*1024)
+            buf = r.raw.read(16 * 1024)
             if not buf:
                 break
             skema += buf.decode()
