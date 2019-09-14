7f0301cd8a870ca8a04af0d3ae09aed998bcf439
remorses <beats.by.morse@gmail.com>
2019-09-14 15:09:14 +0200

    tartiflette_scalars is external

--- mongoke/templates/scalars.py
+++ mongoke/templates/scalars.py
@@ -1,60 +1,19 @@
 scalars_implementations = '''
 from tartiflette import Scalar
-from bson import ObjectId
 from typing import Union
 from tartiflette.language.ast.base import Node
 from tartiflette.constants import UNDEFINED_VALUE
+from tartiflette_scalars import Json, ObjectId, AnyScalar
 
 
 JsonScalar = Scalar("Json")
-@JsonScalar
-class JsonClass:
-    @staticmethod
-    def coerce_input(val):
-        return val
-
-    @staticmethod
-    def coerce_output(val):
-        return val
-
-    def parse_literal(self, ast: "Node") -> Union[str, "UNDEFINED_VALUE"]:
-        return self.coerce_input(ast.value)
+JsonScalar(Json)
 
 AnyScalarScalar = Scalar("AnyScalar")
-@AnyScalarScalar
-class AnyScalarClass:
-    @staticmethod
-    def coerce_input(val):
-        if val == 'true':
-            return True
-        elif val == 'false':
-            return False
-        else:
-            try:
-                return float(val)
-            except Exception:
-                return str(val)
-
-    @staticmethod
-    def coerce_output(val):
-        return val
-
-    def parse_literal(self, ast: "Node") -> Union[str, "UNDEFINED_VALUE"]:
-        return self.coerce_input(ast.value)
+AnyScalarScalar(AnyScalar)
 
 ObjectIdScalar = Scalar("ObjectId")
-@ObjectIdScalar
-class ObjectIdClass:
-    @staticmethod
-    def coerce_input(val):
-        return ObjectId(val)
-
-    @staticmethod
-    def coerce_output(val):
-        return str(val)
-
-    def parse_literal(self, ast: "Node") -> Union[str, "UNDEFINED_VALUE"]:
-        return self.coerce_input(ast.value)
+ObjectIdScalar(ObjectId)
 
 
 ${{
