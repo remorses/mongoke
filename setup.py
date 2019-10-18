d4b7fa26e1dc608671914c57b7b8979c45713919
bump-version <bump@version.com>
2019-10-18 12:12:41 +0000

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ NAME = 'mongoke'
 setup(
     name=NAME,
     # [bump]
-    version='0.0.1',
+    version='0.0.2',
     description=NAME,
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
