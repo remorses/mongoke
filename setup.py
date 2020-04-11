430f3a8bd50488a49994f2fa2b8ff63f18e96d84
bump-version <bump@version.com>
2020-04-11 00:57:35 +0000

    Version 1.4.2 [skip ci]

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ NAME = 'mongoke'
 setup(
     name=NAME,
     # [bump]
-    version='1.4.1',
+    version='1.4.2',
     description=NAME,
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
