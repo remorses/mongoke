1eb1158a3e87948079d16f7f28682a3b7176c212
remorses <beats.by.morse@gmail.com>
2020-05-12 12:03:56 +0200

    web-ui/pages/index:

--- web-ui/pages/index.tsx
+++ web-ui/pages/index.tsx
@@ -1,13 +1,19 @@
 import GraphiQL from 'graphiql'
 import { Box } from '@chakra-ui/core'
 
+
+
+const API_URL = process.env.NODE_ENV == "production" ? "/" : "http://localhost:8080"
+
+
+
 const Page = (props) => {
     return (
         <Box height='100%'>
             <GraphiQL
                 fetcher={async (graphQLParams) => {
                     const data = await fetch(
-                        '/',
+                        API_URL,
                         {
                             method: 'POST',
                             headers: {
